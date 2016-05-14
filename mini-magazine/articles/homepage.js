e2818ce90d5b740c7b30edb395a976d5c21a93a7
vmorales215 <victor.morales1990@gmail.com>
2016-05-13 20:14:18 -0400

    site revamp
    additional changes

---
+++ mini-magazine/articles/homepage.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById("mysidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
